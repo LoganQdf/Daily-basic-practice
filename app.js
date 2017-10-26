4076ffc1716c58b0a8f63a8f465f1ab31f4adecb
LoganQdf <30816786+LoganQdf@users.noreply.github.com>
2017-10-26 19:25:06 +0800

    this is head portrait upload

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes');
-
+var routes = require('./routes');
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(index);
+app.use('/', routes);
 
 
 // catch 404 and forward to error handler
@@ -42,7 +42,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.listen(3000,()=>{
-  console.log('node is OK')
+app.listen(3000,function () {
+    console.log('node is ok');
 })
 module.exports = app;
